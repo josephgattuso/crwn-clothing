3d8d35acf10f72f375203db77e2dae1959a15eb5
Joseph Gattuso <joseph.gattuso@gmail.com>
2021-01-11 20:35:23 +0000

    update header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -16,6 +16,9 @@ const Header = () => (
       <Link className="option" to="/contact">
         CONTACT
       </Link>
+      <Link className="option" to="/signin">
+        SIGN IN
+      </Link>
     </div>
   </div>
 );
