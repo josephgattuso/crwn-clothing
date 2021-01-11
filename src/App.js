3b866fc9c1ff67a7480b14b510122caa920762e6
Joseph Gattuso <joseph.gattuso@gmail.com>
2021-01-11 16:52:33 +0000

    create pages folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
-
-import HomePage from "./homepage.component";
-
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <HomePage />
-      </div>
-    );
-  }
+import HomePage from "./pages/homepage/homepage.component";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
