7da775da2623b7d3ba188ef300c4c54ca8e9d2ea
Joseph Gattuso <joseph.gattuso@gmail.com>
2021-01-12 15:36:19 +0000

    implement userReducer and userActions to header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,12 @@
+// see https://react-redux.js.org/api/connect
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -15,7 +18,7 @@ const Header = ({ currentUser }) => (
       <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link className="option" to="/contact">
+      <Link className="option" to="/shop">
         CONTACT
       </Link>
       {currentUser ? (
@@ -31,4 +34,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
