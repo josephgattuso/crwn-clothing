907edc5dd42fe2b1945dd3b18a9a14c60d7385db
Joseph Gattuso <joseph.gattuso@gmail.com>
2021-01-13 13:00:54 +0000

    config local storage session

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,15 @@
 // see https://www.npmjs.com/package/redux
 // see https://www.npmjs.com/package/redux-logger
 // see https://www.npmjs.com/package/react-redux
+// see https://www.npmjs.com/package/redux-persist
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
