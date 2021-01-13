f55f2c0abd7a28527815ad53e8dc9ab93d4ac5f2
Joseph Gattuso <joseph.gattuso@gmail.com>
2021-01-13 15:06:06 +0000

    wip- add stripe integration for testing

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51I9AZzF6Iddl84yE5zjTwBXb6vTzxYcF5dxZPWLXnCUjhd2jVcgEfI58oxed0caQFbTHlDDJTyUwMbbUSEFtbL7d00qShfGZ3Z';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Demo"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
