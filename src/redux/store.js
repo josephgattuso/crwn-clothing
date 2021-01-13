96c22e3094cd57b8fd7701f62621e4359cab1ee1
Joseph Gattuso <joseph.gattuso@gmail.com>
2021-01-13 14:29:03 +0000

    update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,3 @@
-// see https://www.npmjs.com/package/redux
-// see https://www.npmjs.com/package/redux-logger
-// see https://www.npmjs.com/package/react-redux
-// see https://www.npmjs.com/package/redux-persist
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
@@ -9,6 +5,10 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
